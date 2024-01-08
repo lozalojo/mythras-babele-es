17255b3afd709e48e7750e70f8ebd7d198b8854b
José E. Lozano <lozanoje@gmail.com>
2024-01-08 23:55:42 +0100

    fix

---
+++ fvtt-module-mythras-es.js
@@ -0,0 +1,3 @@
+Hooks.once('init', () => {
+	document.getElementById("logo").src="/modules/fvtt-module-mythras-es/images/fvtt-mythras-es.webp";
+});
--- mythras-es-register.js
+++
@@ -1,3 +0,0 @@
-Hooks.once('init', () => {
-	document.getElementById("logo").src="/modules/fvtt-module-mythras-es/img/fvtt-mythras-es.webp";
-});
